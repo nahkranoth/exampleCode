d7231449e25d451eb43018fa34d470d8b958b886
nahkranoth <joey@nahkranoth.nl>
2019-11-28 23:51:18 +0100

    Added cards

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,57 @@
 import * as PIXI from "pixi.js";
+import * as path from 'path'
 
 class App {
 
     private app: PIXI.Application;
+    private cardAmount = 5;
+    private xCenter;
+    private yCenter;
+    private xCenterOffset = -200;
+    private yCenterOffset = 0;
+    private xOffset = -2;
+    private yOffset = -2;
 
     constructor() {
         this.app = new PIXI.Application({ width: 640, height: 360, antialias: true });
         document.addEventListener("DOMContentLoaded", () => this.afterInit(), false);
     }
 
-    private afterInit() {
-        document.body.appendChild(this.app.view);
-        this.app.stage.sortableChildren = true;
-
-
-        const circle = new PIXI.Graphics();
-        circle.beginFill(0x5cafe2);
-        circle.drawCircle(0, 0, 80);
-        circle.x = 320;
-        circle.y = 180;
-        circle.zIndex = 0;
-        this.app.stage.addChild(circle);
-
-        const circle2 = new PIXI.Graphics();
-        circle2.beginFill(0x443344);
-        circle2.drawCircle(0, 0, 80);
-        circle2.x = 220;
-        circle2.y = 180;
-        circle2.zIndex = 1;
-
-        this.app.stage.addChild(circle2);
+    private afterInit(){
+        var loader = new PIXI.Loader();
+        loader.add("card",'img/playcard.png');
+        loader.load((loader, resources) => {this.afterPreload(loader, resources)});
     }
 
+    private afterPreload(loader, resources) {
+        console.log("Preloaded");
+        this.xCenter = this.app.renderer.width / 2;
+        this.yCenter = this.app.renderer.height / 2;
+        document.body.appendChild(this.app.view);
+        this.app.stage.sortableChildren = true;
+        for(var i=0;i<this.cardAmount;i++){
+            this.createCard(i, resources.card.texture);
+        }
+    }
+
+    private createCard(index, image){
+        const position = this.getStartPositionFromIndex(index);
+        //TODO: preload Image
+        const card = PIXI.Sprite.from(image);
+        card.width = 100;
+        card.height = 140;
+        card.anchor.x = 0.5;
+        card.anchor.y = 0.5;
+        card.zIndex = index;
+        card.x = position.x;
+        card.y = position.y;
+        this.app.stage.addChild(card);
+    }
+
+    private getStartPositionFromIndex(index){
+        const x = this.xCenter + index * this.xOffset + this.xCenterOffset;
+        const y = this.yCenter + index * this.yOffset + this.yCenterOffset;
+        return {x:x, y:y}
+    }
 }
 const a = new App();
